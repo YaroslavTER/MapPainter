a8db15413f6216fe64e2a96492317b7fe32b626e
Oleksandr Dubenko <odybenko@gmail.com>
2017-05-06 22:47:19 +0200

    stuff

--- Painter.js
+++ Painter.js
@@ -3,29 +3,52 @@ var ctx = canvas.getContext('2d')
 
 const PART_NUMBER = 10
 const FIELD_HEIGHT = 100
-const PART_HEIGHT = FIELD_HEIGHT/PART_NUMBER
+const PART_HEIGHT = FIELD_HEIGHT / PART_NUMBER
 
 var map = [
-			['g', 'g', 'p', 'g', 'g', 'p', 'g', 'g'],
-			['g', 'g', 'p', 'g', 'g', 'p', 'g', 'g'],
-			['g', 'g', 'p', 'p', 'p', 'p', 'g', 'g'],
-			['p', 'p', 'p', 'w', 'w', 'p', 'g', 'g'],
-			['g', 'g', 'p', 'w', 'w', 'p', 'g', 'g'],
-			['g', 'g', 'p', 'w', 'w', 'p', 'g', 'g'],
-			['w', 'g', 'p', 'w', 'w', 'p', 'g', 'g'],
-			['w', 'g', 'p', 'p', 'p', 'p', 'g', 'g'],
-			['w', 'g', 'g', 'g', 'g', 'p', 'g', 'g'],
-			['g', 'g', 'g', 'g', 'g', 'p', 'g', 'g'],
-		  ]
+	['g', 'g', 'p', 'g', 'g', 'p', 'g', 'g'],
+	['g', 'g', 'p', 'g', 'g', 'p', 'g', 'g'],
+	['g', 'g', 'p', 'p', 'p', 'p', 'g', 'g'],
+	['p', 'p', 'p', 'w', 'w', 'p', 'g', 'g'],
+	['g', 'g', 'p', 'w', 'w', 'p', 'g', 'g'],
+	['g', 'g', 'p', 'w', 'w', 'p', 'g', 'g'],
+	['w', 'g', 'p', 'w', 'w', 'p', 'g', 'g'],
+	['w', 'g', 'p', 'p', 'p', 'p', 'g', 'g'],
+	['w', 'g', 'g', 'g', 'g', 'p', 'g', 'g'],
+	['g', 'g', 'g', 'g', 'g', 'p', 'g', 'g'],
+]
+
+const getRandom = array => array[~~(Math.random() * (array.length - 1)) + 1]
 
 function drawMap() {
-	let rowLength = map.length
-	for(let rowIndex = 0; rowIndex < rowLength; rowIndex++) {
-		let colLength = map[rowIndex].length
-		let line = map[rowIndex]
-		for(let colIndex = 0; colIndex < colLength; colIndex++) {
-			let element = line[colIndex]
-			choiseColor(element, colIndex*FIELD_HEIGHT, rowIndex*FIELD_HEIGHT)
+	const height = map.length
+	const width = map[0].length
+
+	for (let x = 0; x < height; x++) {
+		for (let y = 0; y < width; y++) {
+			const type = map[x][y]
+			drawTile(type, y * FIELD_HEIGHT, x * FIELD_HEIGHT, tiles[type])
+			// choiseColor(type, y * FIELD_HEIGHT, x * FIELD_HEIGHT)
+		}
+	}
+}
+
+const tiles = {
+	g: ["#0e7f03", '#006010', '#1cb235', '#0bed31'],
+	p: ["#a5a200", '#e0dc18', '#f7f44a', '#edea68'],
+	w: ["#0d8ac4", '#60c4f2', '#c2e8f9', '#268fbf']
+}
+
+function drawTile(type, px, py, colors) {
+	ctx.fillStyle = colors[0];
+	ctx.fillRect(px, py, FIELD_HEIGHT, FIELD_HEIGHT);
+	let x, y;
+	for (x = 0; x < PART_HEIGHT; ++x) {
+		for (y = 0; y < PART_HEIGHT; ++y) {
+			if (Math.random() > 0.66) {
+				ctx.fillStyle = colors[~~(Math.random() * 3) + 1]
+				ctx.fillRect(px + x * PART_HEIGHT, py + y * PART_HEIGHT, PART_HEIGHT, PART_HEIGHT);
+			}
 		}
 	}
 }
@@ -49,11 +72,14 @@ function generateParticles(inputColor, x, y) {
 	let particles = []
 	let xPosition = 0
 	let yPosition = 0
-	for(let rowIndex = 0; rowIndex < PART_NUMBER; rowIndex++) {
-		for(let colIndex = 0; colIndex < PART_NUMBER; colIndex++) {
-			if(randFor(0, 3) == 0)
-				particles.push( {color: inputColor, x: xPosition + x,
-					                                y: yPosition + y} )
+	for (let rowIndex = 0; rowIndex < PART_NUMBER; rowIndex++) {
+		for (let colIndex = 0; colIndex < PART_NUMBER; colIndex++) {
+			if (randFor(0, 3) == 0)
+				particles.push({
+					color: inputColor,
+					x: xPosition + x,
+					y: yPosition + y
+				})
 			xPosition += PART_HEIGHT
 		}
 		xPosition = 0
@@ -73,17 +99,20 @@ function randFor(min, max) {
 function choiseColor(element, x, y) {
 	let mainColor
 	let colors
-	if(element == 'g') {
+	if (element == 'g') {
 		mainColor = '#0e7f03'
 		colors = ['#006010', '#1cb235', '#0bed31']
-	} else if(element == 'p') {
+	} else if (element == 'p') {
 		mainColor = '#a5a200'
 		colors = ['#e0dc18', '#f7f44a', '#edea68']
- 	} else if(element == 'w'){
+	} else if (element == 'w') {
 		mainColor = '#0d8ac4'
 		colors = ['#60c4f2', '#c2e8f9', '#268fbf']
- 	}
-	drawField({x: x, y: y}, mainColor, colors)
+	}
+	drawField({
+		x: x,
+		y: y
+	}, mainColor, colors)
 }
 
 ctx.filter = 'contrast(75%)'
