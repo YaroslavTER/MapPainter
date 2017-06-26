b180da6158aca5293f12bb83aab5820eb9b720dc
Yaroslav <yaroslavtereshchuk@gmail.com>
2017-06-26 16:33:28 +0300

    qwerty

--- ts/src/Canvas/Canvas.ts
+++ ts/src/Canvas/Canvas.ts
@@ -25,28 +25,6 @@ function Canvas() {
     }
   }
 
-  function calculateCurrentPosition(minimapState: MapState) {
-    let mSize = 2;
-    let widthToSubtract = 0;
-    let heightToSubtract = 0;
-    let size = settings.cell.size;
-    let height = mSize*settings.canvas.height/size;
-    let width = mSize*settings.canvas.width/size;
-    let mWidth = settings.width*mSize;
-    let mHeight = settings.height*mSize;
-    let xDisp = mod(minimapState.position.x*mSize, mWidth);
-    let yDisp = mod(minimapState.position.y*mSize, mHeight);
-    let x = xDisp/size*size;
-    let y = (yDisp + settings.canvas.height -
-      settings.height*mSize)/size*size;
-    if(xDisp > mWidth - width)
-      widthToSubtract = Math.abs(mWidth - width - xDisp);
-    if(yDisp > mHeight - height)
-      heightToSubtract = Math.abs(mHeight - height - yDisp);
-    /*drawCurrentPosition(x, y, mSize, width, height,
-      widthToSubtract, heightToSubtract);*/
-  }
-
   function drawCurrentPosition(currentPosition: CurrentPoition) {
     let x = currentPosition.position.x;
     let y = currentPosition.position.y;
